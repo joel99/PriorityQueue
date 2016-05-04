4a6d977acef3bcfd18da620e3d22eb47115a2ca8
Joel Ye <jye5@stuy.edu>
2016-05-04 19:15:06 -0400

    Fix the dang generics

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -1,13 +1,16 @@
+//<Team Name> -- Joel Ye and Advay Sriram
+//APCS2 pd10
+//HW35 -- Privileged Status Gets You to the Front of the Line
+//2016 - 05 - 04
+
 import java.util.ArrayList;
 
-public class ArrayPriorityQueue<T> implements PriorityQueue{
+public class ArrayPriorityQueue<T extends Comparable<T>> implements PriorityQueue<T>{
     
     private ArrayList<T> _arrayList;
 
     public ArrayPriorityQueue(){
-	
-	_arrayList = new ArrayList<T>();
-
+		_arrayList = new ArrayList<T>();
     }
 
     public boolean isEmpty(){
@@ -15,27 +18,37 @@ public class ArrayPriorityQueue<T> implements PriorityQueue{
     }
 
     public void add(T x){
-	_arrayList.add(x);
+		_arrayList.add(x);
     }
 
     public T peekMin(){
         int lowest = 0;
 	for (int i = 1; i < _arrayList.size(); i++){
-	    if (_arrayList.get(lowest).compareTo(_arrayList.get(i)) > 0){
+	    if (((Comparable)(_arrayList.get(lowest))).compareTo((Comparable)_arrayList.get(i)) > 0){
 		lowest = i;
 	    }
 	}
-	_arrayList.get(lowest);
+	return _arrayList.get(lowest);
 	
     }
 
     public T removeMin(){
         int lowest = 0;
 	for (int i = 1; i < _arrayList.size(); i++){
-	    if (_arrayList.get(lowest).compareTo(_arrayList.get(i)) > 0){
+	    if (((Comparable)(_arrayList.get(lowest))).compareTo((Comparable)_arrayList.get(i)) > 0){
 		lowest = i;
 	    }
 	}
-	_arrayList.remove(lowest);
+	return _arrayList.remove(lowest);
     }
+	
+	public static void main(String[] args){
+		ArrayPriorityQueue<Integer> arr = new ArrayPriorityQueue<>();
+		arr.add(1);
+		arr.add(3);
+		arr.add(2);
+		System.out.println(arr.peekMin());
+		System.out.println(arr.removeMin());
+		System.out.println(arr.removeMin());
+	}
 }
